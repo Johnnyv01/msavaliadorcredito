b845390d218b8cfe3155f70a0a4b2398afb9343d
Jonathan Vicente Mota <jonathan-priston@hotmail.com>
2023-07-30 18:42:20 -0300

    first commit

---
+++ src/test/java/course/wsavaliadorcredito/msavaliadorcredito/MsavaliadorcreditoApplicationTests.java
@@ -0,0 +1,13 @@
+package course.wsavaliadorcredito.msavaliadorcredito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsavaliadorcreditoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
